92d057c53885ded453691b2e69c106ff0464e728
Ubuntu <ubuntu@ip-172-31-46-56.eu-north-1.compute.internal>
2024-01-28 13:37:54 +0000

    working-version-1.0

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,44 +1,44 @@
 // server here
-import Express from "express";
-import cors from "cors";
-import passport from "passport";
+//import Express from "express";
+//import cors from "cors";
+//import passport from "passport";
 
-import productsRouter from "./routes/products";
-import usersRouter from "./routes/users";
-import ordersRouter from "./routes/orders";
-import apiErrorHandler from "./middlewares/apiErrorHandler";
-import { jwtStrategy } from "./config/passport";
-import path from "path";
-import { Request, Response } from "express";
+//import productsRouter from "./routes/products";
+//import usersRouter from "./routes/users";
+//import ordersRouter from "./routes/orders";
+//import apiErrorHandler from "./middlewares/apiErrorHandler";
+//import { jwtStrategy } from "./config/passport";
+//import path from "path";
+//import { Request, Response } from "express";
 
-const app = Express();
-const _dirname = path.dirname("");
-const buildpath = path.join(_dirname, "../client/build");
+//const app = Express();
+//const _dirname = path.dirname("");
+//const buildpath = path.join(_dirname, "../client/build");
 
-app.use(Express.static(buildpath));
+//app.use(Express.static(buildpath));
 
-app.use(Express.json());
-app.use(cors());
-app.use(passport.initialize());
-passport.use(jwtStrategy);
-app.get("/", function(req:Request, res:Response){
+//app.use(Express.json());
+//app.use(cors());
+//app.use(passport.initialize());
+//passport.use(jwtStrategy);
+//app.get("/", function(req:Request, res:Response){
 
-  res.sendFile(
-      path.join(__dirname, "../client/build/index.html"),
-      function (err) {
-        if (err) {
-          res.status(500).send(err);
-        }
-      }
-    );
+//  res? res.sendFile(
+//      path.join(__dirname, "../client/build/index.html"),
+//      function (err) {
+//        if (err) {
+//          res.status(500).send(err);
+//        }
+//      }
+//    ): null;
 
-})
-app.use("/products", productsRouter);
-app.use("/users", usersRouter);
-app.use("/orders", ordersRouter);
+//})
+//app.use("/products", productsRouter);
+//app.use("/users", usersRouter);
+//app.use("/orders", ordersRouter);
 
-app.use(apiErrorHandler);
+//app.use(apiErrorHandler);
 
 
 
-export default app;
+//export default app;
--- api/src/server.ts
+++ api/src/server.ts
@@ -2,10 +2,49 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-import app from "./app";
-import path from "path";
 import Express from "express";
+import cors from "cors";
+import passport from "passport";
+
+import productsRouter from "./routes/products";
+import usersRouter from "./routes/users";
+import ordersRouter from "./routes/orders";
+import apiErrorHandler from "./middlewares/apiErrorHandler";
+import { jwtStrategy } from "./config/passport";
+import path from "path";
 import { Request, Response } from "express";
+
+
+const app = Express();
+const _dirname = path.dirname("");
+const buildpath = path.join(_dirname, "../client/build");
+
+app.use(Express.static(buildpath));
+
+app.use(Express.json());
+app.use(cors());
+app.use(passport.initialize());
+passport.use(jwtStrategy);
+app.get("/", function(req:Request, res:Response){
+
+ res.sendFile(
+      path.join(__dirname, "../client/build/index.html"),
+      function (err) {
+        if (err) {
+          res.status(500).send(err);
+        }
+      }
+    );
+
+})
+app.use("/products", productsRouter);
+app.use("/users", usersRouter);
+app.use("/orders", ordersRouter);
+
+app.use(apiErrorHandler);
+
+
+
 
 dotenv.config();
 
